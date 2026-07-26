981373d827752d098b1cbfdae02b191597611bfc
Myriam LERIDER <minimir1@yahoo.fr>
2026-07-26 19:15:13 -0400

    Correction des routes pour les articles et ajout des routes d'édition et de mise à jour dans l'interface admin

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,14 @@ Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])-
 
 
 //Route pour Article
-Route::get('articles',Route::get('/admin/articles', [ArticleController::class, 'adminIndex'])->name('admin-articles-list');
+Route::get('articles', [ArticleController::class, 'index'])->name('articles-list');
+
 
 //Route pour admin-article
+Route::get('/admin/articles', [ArticleController::class, 'adminIndex'])->name('admin-articles-list');
 Route::get('/admin/articles/create', [ArticleController::class, 'create'])->name('admin-articles-create');
 Route::post('/admin/articles', [ArticleController::class, 'store'])->name('admin-articles-store');
+Route::get('/admin/articles/{article}/edit', [ArticleController::class, 'edit'])->name('admin-articles-edit');
+Route::put('/admin/articles/{article}', [ArticleController::class, 'update'])->name('admin-articles-update');
 Route::delete('/admin/articles/{article}', [ArticleController::class, 'destroy'])->name('admin-articles-destroy');
-Route::patch('/admin/articles/{article}/publish', [ArticleController::class, 'publish'])->name('admin-articles-publish'); [ArticleController::class, 'index'])->name('articles-list');
+Route::patch('/admin/articles/{article}/publish', [ArticleController::class, 'publish'])->name('admin-articles-publish');
